52f901175af51b553e77974d30e95961d0b24240
Cathella <ncathie214@gmail.com>
2022-01-06 18:15:12 +0300

    Update Jobs component

--- src/components/jobs/Jobs.js
+++ src/components/jobs/Jobs.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getJobs, jobsList } from "../../redux/jobs/jobs";
+import Job from '/Job';
 
-const Jobs = () => (
-  <div>Hello This is the Jobs component</div>
-);
+const Jobs = () => {
+  const jobs = useSelector((state) => state.jobs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    jobsList().then((data) => dispatch(getJobs(data)));
+  });
+
+  return (
+    <div>
+      {
+        jobs.map((job) => (
+          <Job
+            key={jobs.id}
+            data={job}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 export default Jobs;
